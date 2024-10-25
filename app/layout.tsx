f5bb8456a81766b8a8b1225d3e1828aa42356ecb
trungth <trungth@vnpt-technology.vn>
2024-10-25 09:57:19 +0700

    rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
@@ -7,7 +9,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
+  title: "Trung June's Portfolio",
   description: "Modern & Minimal JS Mastery Portfolio",
 };
 
@@ -29,6 +31,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
